33d6f78c022c89523bf487ea7d822b63a508fd76
Thierry-Chain <irambonat0@gmail.com>
2021-07-05 15:26:56 +0300

    Co-authored-by: Iramboana-Thierry <irambonat0@gmail.com>

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,20 +2,19 @@
 //import axios from 'axios'
 import React from 'react'
 import { useQuery } from 'react-query'
-/*
+
 let getData = async () => {
   res = await fetch('http://localhost:3000/posts')
   res.json()
 }
-*/
 
 export default function Main() {
-  // let { data, status } = useQuery('post', getData)
-  //console.log(data)
+  let { data, status } = useQuery('post', getData)
+  console.log(data)
   return (
     <React.Fragment>
       <div className="main">Main content</div>
-      <p>status:{}</p>
+      <p>status:{status}</p>
     </React.Fragment>
   )
 }
